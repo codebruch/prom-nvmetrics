f1e1abc0336b88e4bf1f342320f88d8abb8f3d7f
codebruch <alexander.bruch@sap.com>
2018-01-14 17:46:07 +0100

    Changed to py3nvml and added dynamic helper naming of metrics

--- prom_nvmetrics.py
+++ prom_nvmetrics.py
@@ -4,66 +4,84 @@ import time
 import xml.etree.ElementTree as ET
 import py3nvml.nvidia_smi as smi
 
-gTemperature = Gauge('GPU_Temperature', 'Description of gauge')
-gfan_speed = Gauge('GPU_fan_speed', 'Fan Speed of GPU: ')
-gfb_memory_usage = Gauge('GPU_memory_usage', 'Memory usage of GPU: ')
+#initialize GPUs to get dynamic name assignment for metric description
+def initialize_gpus():
+       out = smi.XmlDeviceQuery()
+       root = ET.fromstring(out)
+       for gpu in root.getiterator('gpu'):
+           gpuName =(gpu.find('product_name').text)
+           print(gpuName)
+           gTemperature = Gauge('GPU_Temperature', 'Temperature of GPU: ' + gpuName)
+           gfan_speed = Gauge('GPU_fan_speed', 'Fan Speed of GPU: '+ gpuName)
+           gfb_memory_usage = Gauge('GPU_memory_usage', 'Memory usage of GPU: ' + gpuName)
+           gfb_memory_total = Gauge('GPU_memory_total', 'Total Memory usage of GPU: ' + gpuName)
+           ggraphics_clock = Gauge('GPU_graphics_clock', 'Graphics clock of GPU: ' + gpuName)
+           gmem_clock = Gauge('GPU_memory_clock', 'Memory clock of GPU: ' + gpuName)
+           gpower_draw = Gauge('GPU_power_draw', 'Power draw of GPU: ' + gpuName)
+           ggpu_util = Gauge('GPU_util', 'Utilization of GPU: ' + gpuName)
+       return  {'gTemperature': gTemperature, 'gfan_speed': gfan_speed,'gfb_memory_usage': gfb_memory_usage,
+                'ggraphics_clock': ggraphics_clock, 'gpower_draw': gpower_draw, 'ggpu_util': ggpu_util,
+                'gfb_memory_total': gfb_memory_total,'gmem_clock': gmem_clock}
 
-# Create a metric to track time spent and requests made.
-#REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
 
-# Decorate function with metric.
-#@REQUEST_TIME.time()
-def execute_and_read_from_SMI(t):
+def execute_and_read_from_SMI(metrics):
     """A dummy function that takes some time."""
-    #time.sleep(t)
-    #si = subprocess.STARTUPINFO()
-    #si.dwFlags =  subprocess.SW_HIDE
-    #out = subprocess.Popen(['C:\\Program Files\\NVIDIA Corporation\\NVSMI\\nvidia-smi.exe', '-q', '-x'  ], stdout=subprocess.PIPE, shell=False, startupinfo=si).communicate()[0]
+
+    gTemperature = metrics.get('gTemperature', 'default')
+    gfan_speed = metrics.get('gfan_speed', 'default')
+    gfb_memory_usage = metrics.get('gfb_memory_usage', 'default')
+    gfb_memory_total = metrics.get('gfb_memory_total', 'default')
+    ggraphics_clock = metrics.get('ggraphics_clock', 'default')
+    gpower_draw = metrics.get('gpower_draw', 'default')
+    ggpu_util = metrics.get('ggpu_util', 'default')
+    gmem_clock = metrics.get('gmem_clock', 'default')
+
     out = smi.XmlDeviceQuery()
     root = ET.fromstring(out)
     print(out)
     for gpu in root.getiterator('gpu'):
-        gpuName =(gpu.find('product_name').text)
-        print(gpuName)
-        #gTemperature
+                         
         temperature=[float(gpu.find('temperature/gpu_temp').text.split()[0])]
-        print(temperature[0])
+        #print(temperature[0])
         gTemperature.set(temperature[0])
 
-        fan_speed = values=[float(gpu.find('fan_speed').text.split()[0])]
+        fan_speed = [float(gpu.find('fan_speed').text.split()[0])]
         gfan_speed.set(fan_speed[0])
 
-        #       vl.dispatch(type='memory', type_instance='used',
-        fb_memory_usage = values=[1e6 * float(gpu.find('fb_memory_usage/used').text.split()[0])]
+        
+        fb_memory_usage = [1e6 * float(gpu.find('fb_memory_usage/used').text.split()[0])]
         gfb_memory_usage.set(fb_memory_usage[0])
         
-        #        vl.dispatch(type='memory', type_instance='total',
-        fb_memory_total = values=[1e6 * float(gpu.find('fb_memory_usage/total').text.split()[0])]
+        
+        fb_memory_total = [1e6 * float(gpu.find('fb_memory_usage/total').text.split()[0])]
+        gfb_memory_total.set(fb_memory_total[0])
+        
+       
+        gpu_util = [float(gpu.find('utilization/gpu_util').text.split()[0])]
+        ggpu_util.set(gpu_util[0])
+     
+        power_draw = [float(gpu.find('power_readings/power_draw').text.split()[0])]
+        print(power_draw)
+        gpower_draw.set(power_draw[0])
 
-        #        vl.dispatch(type='percent', type_instance='gpu_util',
-        gpu_util = values=[float(gpu.find('utilization/gpu_util').text.split()[0])]
-        #       vl.dispatch(type='power', type_instance='draw',
-        power_draw = values=[float(gpu.find('power_readings/power_draw').text.split()[0])]
+        
+        graphics_clock = [float(gpu.find('clocks/graphics_clock').text.split()[0])]
+        ggraphics_clock.set(graphics_clock[0])
+        
 
-
-        #        vl.dispatch(type='cpufreq', type_instance='graphics_clock',
-        graphics_clock = values=[float(gpu.find('clocks/graphics_clock').text.split()[0])]
-
-        #vl.dispatch(type='cpufreq', type_instance='sm_clock',
-        sm_clock = values=[float(gpu.find('clocks/sm_clock').text.split()[0])]
-
-        #vl.dispatch(type='cpufreq', type_instance='mem_clock',
-        mem_clock = values=[float(gpu.find('clocks/mem_clock').text.split()[0])]
-
+        mem_clock = [float(gpu.find('clocks/mem_clock').text.split()[0])]
+        gmem_clock.set(mem_clock[0])
 
 
 
 
 if __name__ == '__main__':
+    #initialize the metrics from GPU
+    metrics = initialize_gpus()
     # Start up the server to expose the metrics.
     start_http_server(8000)
     # Generate some requests.
     while True:
         time.sleep(2)
-        execute_and_read_from_SMI(random.random())
+        execute_and_read_from_SMI(metrics)
         
